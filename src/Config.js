2b818b84b38d6a8813a210552906fe7fde3a6241
Asleshakan <142888325+Asleshakan@users.noreply.github.com>
2024-08-30 12:07:07 +0530

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 export const config = {
     appId: '0ee86360-889f-4a30-b1c0-0532a6a99a2c',
-    redirectUri: 'http://localhost:3000',
+    redirectUri: 'https://wonderful-plant-04e7a460f.5.azurestaticapps.net/',
     scopes: ['openid', 'profile'],
     authority: 'https://kazureb2c.b2clogin.com/kazureb2c.onmicrosoft.com/B2C_1_SignUpSingIn',
     knownAuthorities: ['https://kazureb2c.b2clogin.com/kazureb2c.onmicrosoft.com/B2C_1_SignUpSingIn'],
